4e5c85a5ad4d5d61a7e685a6d711f43c108dfe59
Kevin Parham <kevin.parham@aa.comgit config --global user.email kevin.parham@aa.com>
2020-08-31 18:47:05 -0500

    adding skills screen

--- swoosh/app/src/main/java/com/example/swoosh/WelcomeActivity.kt
+++ swoosh/app/src/main/java/com/example/swoosh/WelcomeActivity.kt
@@ -1,16 +1,15 @@
 package com.example.swoosh
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_league.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        getStartedBtn.setOnClickListener {
+        leagueNextBtn.setOnClickListener {
             val leagueIntent = Intent(this, LeagueActivity::class.java)
             startActivity(leagueIntent)
         }
