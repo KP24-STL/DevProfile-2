181c577ef0653a81c00156d91a46ea04c535be31
Kevin Parham <kevin.parham@aa.comgit config --global user.email kevin.parham@aa.com>
2020-09-09 17:31:19 -0500

    adding code from lessons Simple ListView and Custom List Adapter

---
+++ CoderSwag/app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.coderswag.Controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.coderswag.Adapters.CategoryAdapter
+import com.example.coderswag.Model.Category
+import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : CategoryAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {  //initializing an array adapter
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//this is code that transform the list view to display image and categories
+//        adapter = ArrayAdapter(this,
+//            android.R.layout.simple_list_item_1,
+//            DataService.categories)
+//        categoryListView.adapter = adapter
+
+        adapter = CategoryAdapter(this, DataService.categories)
+
+//        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        categoryListView.adapter = adapter
+    }
+}
---
+++ CoderSwag/app/src/main/java/com/example/coderswag/Services/DataService.kt
@@ -0,0 +1,37 @@
+package com.example.coderswag.Services
+
+import com.example.coderswag.Model.Category
+import com.example.coderswag.Model.Product
+
+object DataService {
+
+//    DATA VIEWS
+    val categories = listOf(
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage")
+    )
+
+    val hats = listOf(
+        Product("Devslopes Graphic Beanie", "$18", "hat1"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat White", "$20", "hat3"),
+        Product("Devslopes Hat Snapback", "$25", "hat4"),
+    )
+
+    val hoodies = listOf(
+        Product("Devslopes Grey Hoodie", "$28", "hoodie1"),
+        Product("Devslopes Hoodie Red", "$32", "hoodie2"),
+        Product("Devslopes Hoodie Grey", "$28", "hoodie3"),
+        Product("Devslopes Hoodie Black", "$32", "hoodie4")
+    )
+
+    val shirts = listOf(
+        Product("Devslopes Shirt Black", "$18", "shirt1"),
+        Product("Devslopes Badge Light Grey", "$15", "shirt2"),
+        Product("Devslopes Logo Shirt Red", "$18", "shirt3"),
+        Product("Devslopes Hustle", "$15", "shirt4"),
+        Product("Kickflip Studios", "$18", "shirt5")
+    )
+}
