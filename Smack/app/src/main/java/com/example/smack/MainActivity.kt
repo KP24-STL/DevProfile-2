9aaf02eb9dd10ef87bc495467a9d271628e938c6
Kevin Parham <kevin.parham@aa.comgit config --global user.email kevin.parham@aa.com>
2020-10-02 12:30:53 -0500

    Setup complete for end of lesson 1

--- Smack/app/src/main/java/com/example/smack/MainActivity.kt
+++ Smack/app/src/main/java/com/example/smack/MainActivity.kt
@@ -1,42 +1,49 @@
 package com.example.smack
 
+import android.app.Activity
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.ActionBarDrawerToggle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.navigation.Navigation.findNavController
+import androidx.core.view.GravityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+//    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+//                val toolbar: Toolbar = findViewById(R.id.toolbar)
 
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+//        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, "Open drawer", "Close drawer")
+//        drawer_layout.addDrawerListener(toggle)
+//        toggle.syncState()
 
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+    }
+
+    private fun ActionBarDrawerToggle(
+        activity: Activity,
+        drawerLayout: DrawerLayout?,
+        toolbar: Toolbar?,
+        s: String,
+        s1: String
+    ): ActionBarDrawerToggle {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onBackPressed() {
+    if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+        drawer_layout.closeDrawer(GravityCompat.START)
+    } else {
+        super.onBackPressed()
+        }
     }
 }
-
